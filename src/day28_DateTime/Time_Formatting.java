4fbb23f97c1f58ab3179ab57ec7f030a405fb1c6
Alikomania <alikoca33@hotmail.com>
2020-04-24 10:36:40 -0500

    DateTime.java

--- src/day28_DateTime/Time_Formatting.java
+++ src/day28_DateTime/Time_Formatting.java
@@ -1,5 +1,23 @@
 package day28_DateTime;
 
-public class TimeFormatting {
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Time_Formatting {
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm:ss a");
+
+        LocalTime time1 = LocalTime.of(16, 30, 25);
+        System.out.println(time1.format(dtf));
+
+        LocalTime time2 = LocalTime.of(12, 0, 45);
+        System.out.println(time2.format(dtf));
+
+        boolean result1 = time1.isAfter(time2);
+        System.out.println(result1);
+
+
+
+    }
 
 }
